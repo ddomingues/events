0f03bd602f1a6193590602bdd41abe8a3f1368c6
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-02-17 01:00:27 -0200

    Use SchedulePage to present the Schedules

--- app/pages/schedules/schedules.ts
+++ app/pages/schedules/schedules.ts
@@ -1,9 +1,13 @@
 import {Page, NavController, NavParams} from 'ionic-framework/ionic';
 
+import {SchedulePage} from './../schedule/schedule'
+
 @Page({
   templateUrl: 'build/pages/schedules/schedules.html'
 })
 export class SchedulesPage {
   constructor(private nav: NavController, navParams: NavParams) {
+    this.tab1 = SchedulePage;
+    this.tab2 = SchedulePage;
   }
 }
