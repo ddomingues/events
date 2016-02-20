d8b721517df8daf303878cf998727fe51e7bf3bf
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-02-20 10:21:54 -0200

    Improve Schedule Page to show the talks received by params

--- app/pages/schedule/schedule.ts
+++ app/pages/schedule/schedule.ts
@@ -1,9 +1,26 @@
 import {Page, NavController, NavParams} from 'ionic-framework/ionic';
+import {OnInit} from 'angular2/core';
+import {Talk} from './../../domain/talk'
+import * as _ from './../../util/util'
 
 @Page({
   templateUrl: 'build/pages/schedule/schedule.html'
 })
-export class SchedulePage {
-  constructor(private nav: NavController, navParams: NavParams) {
+export class SchedulePage implements OnInit {
+  talks: Talk[];
+  schedules: Array<{start_at: string, talks: Talk[]}>;
+
+  constructor(private nav: NavController, private navParams: NavParams) {
+  }
+
+  ngOnInit() {
+    this.talks = this.navParams.get('talks');
+    this.schedules = this.mapSchedule(_.group(this.talks, t => t.start_at.toLocaleTimeString()));
+  }
+
+  mapSchedule(talksByStart) {
+    return Object
+      .keys(talksByStart)
+      .map(d => ({start_at: d, talks: talksByStart[d]}));
   }
 }
