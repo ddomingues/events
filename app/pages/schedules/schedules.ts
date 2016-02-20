fc47e454f755c41bb2b7207b35dea1914054fcfe
Diego Henrique Domingues <diego.domingues16@gmail.com>
2016-02-20 10:20:50 -0200

    Improve Schedules Page to show real Events

--- app/pages/schedules/schedules.ts
+++ app/pages/schedules/schedules.ts
@@ -1,13 +1,41 @@
 import {Page, NavController, NavParams} from 'ionic-framework/ionic';
-
+import {OnInit} from 'angular2/core';
+import {TalkService} from './../../services/talk'
+import {Talk} from './../../domain/talk'
 import {SchedulePage} from './../schedule/schedule'
+import * as _ from './../../util/util'
 
 @Page({
   templateUrl: 'build/pages/schedules/schedules.html'
 })
-export class SchedulesPage {
-  constructor(private nav: NavController, navParams: NavParams) {
-    this.tab1 = SchedulePage;
-    this.tab2 = SchedulePage;
+export class SchedulesPage implements OnInit{
+  schedules: Array<any>;
+
+  constructor(private talkService: TalkService) {
+  }
+
+  ngOnInit() {
+    this.schedules = [];
+
+    this.talkService
+      .all()
+      .subscribe(
+        talks => {
+          this.schedules = this.mapTalksByDay(
+            _.group(talks, t => t.day.toDateString())
+          ).map(this.toTab);
+        },
+        err => console.log(err)
+      );
+  }
+
+  private mapTalksByDay(talksGroupedByDate) {
+    return Object
+      .keys(talksGroupedByDate)
+      .map(d => ({ date: new Date(d), talks: talksGroupedByDate[d] }));
+  }
+
+  private toTab(schedule) {
+    return {params: schedule, page: SchedulePage};
   }
 }
